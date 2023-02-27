3f3e03511650a30f78b78ed79c09a7ef6e74bebd
Vitaliy <sprinter-100m@ukr.net>
2023-02-27 21:33:37 +0200

    added admin

--- movies_tv/admin.py
+++ movies_tv/admin.py
@@ -2,11 +2,60 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(Actor)
-admin.site.register(Reting)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # Категории
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+
+class ReviewInline(admin.StackedInline):
+    # Прикрипление отзывов к фильму
+    model = Reviews
+    readonly_fields = ('name', 'email')
+    extra = 1
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    # Фильмы
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewInline]
+    list_editable = ('draft',)
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    # Отзывы
+    list_display = ('id', 'name', 'email', 'parent', 'movie')
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    # Жанры
+    list_display = ('id', 'name', 'url')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    # Актеры
+    list_display = ('name', 'age')
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    # Кадры из фильма
+    list_display = ('title', 'movie')
+
+
+@admin.register(Reting)
+class RetingAdmin(admin.ModelAdmin):
+    # Рейтинг
+    list_display = ('ip', 'star')
+
+
 admin.site.register(RatingsStar)
-admin.site.register(Reviews)
--- movies_tv/apps.py
+++ movies_tv/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MoviesTvConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies_tv'
+    verbose_name = 'Фильмы'
